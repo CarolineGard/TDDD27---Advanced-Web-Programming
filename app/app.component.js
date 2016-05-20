4d0d92952b54df1112818d1d9fef8a3d6043413e
CarolineGard <caroline.gard@hotmail.com>
2016-05-20 07:44:35 +0200

    Added: Showing list with elements from array, modify elements in real time

--- app/app.component.js
+++ app/app.component.js
@@ -9,17 +9,33 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var BerryPlace = (function () {
+    function BerryPlace() {
+    }
+    return BerryPlace;
+}());
+exports.BerryPlace = BerryPlace;
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Smultronstället';
+        this.berries = BERRIES;
     }
+    AppComponent.prototype.onSelect = function (berry) { this.selectedBerry = berry; };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>Smultronstället</h1>'
+            template: "\n  \t<h1>{{title}}</h1>\n  \t<h2>Alla smultronst\u00E4llen:</h2>\n  \t<ul class=\"berries\">\n  \t\t<li *ngFor=\"let berry of berries\" [class.selected]=\"berry === selectedBerry\" (click)=\"onSelect(berry)\"> \n  \t\t\t<span class=\"badge\">{{berry.id}}</span> {{berry.name}}\n  \t\t</li>\n  \t</ul>\n  \t<div *ngIf=\"selectedBerry\">\n  \t\t<div>\n\t\t \t<label>name: </label>\n\t\t \t<input [(ngModel)]=\"selectedBerry.name\" placeholder=\"name\"><br><br>\n\t\t \t<label>Where is it located? </label>\n\t\t \t<input [(ngModel)]=\"selectedBerry.location\" placeholder=\"name\"><br><br>\n\t\t \t<label>Category: </label>\n\t\t \t<input [(ngModel)]=\"selectedBerry.category\" placeholder=\"name\"><br><br>\n\t\t \t<label>Description: </label>\n\t\t \t<input [(ngModel)]=\"selectedBerry.description\" placeholder=\"name\">\n\t\t \t</div>\n\t\t</div>\n  \t",
+            styles: ["\n  \t\t.selected {\n  \t\t\tbackground-color: #cfd8ca !important;\n  \t\t\tcolor: white;\n  \t\t}\n  \t\t.berries li {\n  \t\t\tcursor: pointer;\n  \t\t\tposition: relative;\n  \t\t\tleft: 0;\n  \t\t}\n  \t\t.berries li.selected:hover {\n  \t\t\tbackground-color: #607D8B;\n  \t\t}\n  \t\t.berries li:hover {\n  \t\t\tbackground-color: #607DaB;\n  \t\t}\n  \t"]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+var BERRIES = [
+    { "id": 1, "name": "Nytorget", "location": "Stockholm", "category": "gata/torg", "description": "Good for sunny days" },
+    { "id": 2, "name": "Hemma", "location": "Kungsholmen", "category": "private", "description": "Best place for a cup of tea" },
+    { "id": 3, "name": "Cafe 60", "location": "Stockholm", "category": "Café/Restaurant", "description": "Cosy cafe located in the heart of the city with the worlds best chocolate cake!" },
+    { "id": 4, "name": "Täppan", "location": "Norrköping", "category": "University", "description": "Cosy study place with cheep coffee" }
+];
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,67 @@
 import { Component } from '@angular/core';
+
+export class BerryPlace {
+	id: number;
+	name: string;
+	location: string;
+	category: string;
+	description: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: '<h1>Smultronstället</h1>'
+  template:`
+  	<h1>{{title}}</h1>
+  	<h2>Alla smultronställen:</h2>
+  	<ul class="berries">
+  		<li *ngFor="let berry of berries" [class.selected]="berry === selectedBerry" (click)="onSelect(berry)"> 
+  			<span class="badge">{{berry.id}}</span> {{berry.name}}
+  		</li>
+  	</ul>
+  	<div *ngIf="selectedBerry">
+  		<div>
+		 	<label>name: </label>
+		 	<input [(ngModel)]="selectedBerry.name" placeholder="name"><br><br>
+		 	<label>Where is it located? </label>
+		 	<input [(ngModel)]="selectedBerry.location" placeholder="name"><br><br>
+		 	<label>Category: </label>
+		 	<input [(ngModel)]="selectedBerry.category" placeholder="name"><br><br>
+		 	<label>Description: </label>
+		 	<input [(ngModel)]="selectedBerry.description" placeholder="name">
+		 	</div>
+		</div>
+  	`,
+  	styles:[`
+  		.selected {
+  			background-color: #cfd8ca !important;
+  			color: white;
+  		}
+  		.berries li {
+  			cursor: pointer;
+  			position: relative;
+  			left: 0;
+  		}
+  		.berries li.selected:hover {
+  			background-color: #607D8B;
+  		}
+  		.berries li:hover {
+  			background-color: #607DaB;
+  		}
+  	`]
 })
-export class AppComponent { }
+
+export class AppComponent { 
+	title = 'Smultronstället';
+	public berries = BERRIES;
+	selectedBerry: BerryPlace;
+	onSelect( berry: BerryPlace) {this.selectedBerry = berry; }
+}
+
+var BERRIES: BerryPlace[] = [
+	{"id": 1, "name": "Nytorget", "location": "Stockholm", "category": "gata/torg", "description": "Good for sunny days"},
+	{"id": 2, "name": "Hemma", "location": "Kungsholmen", "category": "private", "description": "Best place for a cup of tea"},
+	{"id": 3, "name": "Cafe 60", "location": "Stockholm", "category": "Café/Restaurant", "description": "Cosy cafe located in the heart of the city with the worlds best chocolate cake!"},
+	{"id": 4, "name": "Täppan", "location": "Norrköping", "category": "University", "description": "Cosy study place with cheep coffee"}
+];
+
+
